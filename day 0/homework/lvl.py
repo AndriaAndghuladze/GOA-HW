16e3bfb9e938dd183b2b73e592e1e062721315cf
AndriaAndghuladze <andriaand999@.gmail.com>
2024-10-03 17:37:16 +0400

    commit

--- day 0/homework/lvl.py
+++ day 0/homework/lvl.py
@@ -86,9 +86,93 @@ forward(25)
 left(90)
 forward(50)
 
+penup()
+goto(-60, -5)
+pendown()
 
+
+#second house
+begin_fill()
+color("gray")
+speed(6)
+shape("turtle")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+end_fill()
+#end of square
+
+penup()
+goto(-130, -5)
+pendown()
+
+#drawing door
+begin_fill()
+color("yellow")
+forward(120)
+left(90)
+forward(55)
+left(90)
+forward(120)
+end_fill()
+
+penup()
+goto(-125, 130)
+pendown()
+
+#window
+
+color("red")
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)    
+left(90)
+forward(50)
+left(90)
+forward(25)
+left(90)
+forward(50)
+
+penup()
+goto(-205, 130)
+pendown()
+
+#second window
+color("red")
+left(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(25)
+right(90)
+forward(50)
+
+penup()
+goto(-265, 190)
+pendown()
+  
+#start roof
+begin_fill()
+color("black")
+right(30)
+forward(200)
+right(117)
+forward(200)
+end_fill()
 
 
 
 exitonclick()
-
