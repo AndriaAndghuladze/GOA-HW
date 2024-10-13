76be07a7c7b4e0860542013cb599a33876d841aa
AndriaAndghuladze <andriaand999@.gmail.com>
2024-10-14 00:12:30 +0400

    commit

---
+++ day 02/class work/lvl2.py
@@ -0,0 +1,16 @@
+#1
+
+name = "andria"
+surname = "andghuladze"
+age = "13"
+
+print( name + surname + age )
+
+#2
+name = "andria"
+surname = "andghuladze"
+hobby = "swimming"
+age = "13"
+favcar = "BMW M3 Competisen"
+
+print(name + surname + hobby + age + favcar )
