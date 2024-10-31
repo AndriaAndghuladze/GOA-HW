a3fe73278419aa6068f204397232032c5afb4372
AndriaAndghuladze <andriaand999@.gmail.com>
2024-10-31 15:40:31 +0400

    commot

---
+++ day 04/homework/lvl4.py
@@ -0,0 +1,43 @@
+#1
+ #მონაცემთა ინფორმაცია იყოფა ორ ნაწილად
+ # 1.ტექსტური-string,str()
+ # 2.რიცხვითი-integer,int() . ათწილადი float\
+
+
+#2 
+print(10+2)           
+print(124+14)
+print(142+14)
+
+print(10-2)           
+print(124-14)
+print(142-14)
+
+print(10*2)           
+print(124*14)
+print(142*14)
+
+print(10/2)           
+print(14/2)
+print(14/14)
+
+print(14%3)           
+print(9%2)
+print(11%2)
+
+print(2**3)           
+print(9**2)
+print(11**2)
+
+#3
+ # /-ეს არის გაყოფა რომლის დროსაც გამოაქ შედეგი, მაგ. 10/2=5
+ # % პროცენტული გაყოფისას გამოაქ ნაშთი, მაგ. 13%2=1
+
+#4
+mom_age = 47
+dad_age = 47
+grandfather_age = 70
+grandmother_age = 69
+  
+
+print(mom_age  +  dad_age + grandfather_age  +  grandmother_age)
