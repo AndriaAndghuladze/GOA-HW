54a2f72d3b941105c132199d9430ed01320aa9cc
AndriaAndghuladze <andriaand999@.gmail.com>
2024-11-13 17:29:58 +0400

    commit

---
+++ day 03/classwork/lvl3.py
@@ -0,0 +1,11 @@
+name = "Andria"
+surname = "Andghuladze"
+age = 13
+country = "Georgia"
+place = "Muxiani"
+fawcar = "BMW E30"
+hobi = "Swimming"
+color = "black"
+
+
+print("name: " + name + ", surname: " + surname + ", age: " + str(age) + ", country: " + country + ", place: " + place + ", fawcar: " + fawcar + ", hobi: " + hobi + ", color: " + color)
