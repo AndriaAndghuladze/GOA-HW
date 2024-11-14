585cc4836b697fbe830862b33084e2380410d87c
AndriaAndghuladze <andriaand999@gmail.com>
2024-11-14 20:37:11 +0400

    commit

---
+++ day 07/homework/lvl7.py
@@ -0,0 +1,60 @@
+#1
+name = "Andria"
+last_name = "Andghuladze"
+countri = "Georgia"
+car = "Audi"
+age = 13
+city = "Tbilisi"
+color = "black"
+
+
+print(name + " " + last_name + " " + countri + " " + car + " " + str(age) + " " + city + " " + color)
+
+#2
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x + y)
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x + y)
+
+
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x - y)
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x - y)
+
+
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x * y)
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x * y)
+
+
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x / y)
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x / y)
+
+
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x // y)
+
+x = int(input("enter X :"))
+y = int(input("enter Y :"))
+print(x // y)
