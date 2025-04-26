c9b76bef30f5ff02f4b072b3ba39a9b25f5fa76c
AndriaAndghuladze <andriaand999@gmail.com>
2025-04-27 00:20:21 +0400

    commit

---
+++ day 57/classwork/script.js
@@ -0,0 +1,16 @@
+// #1
+let name = prompt('enter your name:');
+let lastName = prompt('enter your lastName:');
+let age = prompt('enter your age:');
+let place = prompt('enter your place:');
+let profesia = prompt('enter your profesion');
+
+console.log('my name is ' + name + 'lastname is ' + lastName + ' age' + age+ 'living in' + place + 'profeson is ' + profesia );
+
+
+// #2
+let total = 200;
+let item = 7;
+let giftBox = total % item;
+
+console.log(giftBox)
---
+++ day 57/homework/script.js
@@ -0,0 +1,110 @@
+// #1
+let name = prompt('enter your name:');
+let lastName = prompt('enter your lastName:');
+let age = prompt('enter your age:');
+let place = prompt('enter your place:');
+let profesia = prompt('enter your profesion');
+
+console.log('my name is ' + name + 'lastname is ' + lastName + ' age' + age+ 'living in' + place + 'profeson is ' + profesia );
+
+
+// #2
+let n1 = Number(prompt('enter num1 :'))
+let n2 = Number(prompt('enter num2 :'))
+let n3 = Number(prompt('enter num3 :'))
+let n4 = Number(prompt('enter num4 :'))
+let n5 = Number(prompt('enter num5 :'))
+
+console.log(n1 + n2 + n3 + n4 + n5)
+console.log(n1 - n2 - n3 - n4 - n5)
+console.log(n1 * n2 * n3 * n4 * n5)
+console.log(n1 / n2 / n3 / n4 / n5)
+console.log(n1 ** n2 ** n3 ** n4 ** n5)
+console.log(n1 % n2 % n3 % n4 % n5)
+
+
+// #3
+let p1 = prompt("hello") 
+let p2 = prompt("hello bro")
+
+console.log(p1)
+console.log(p2)
+
+
+// #4
+let x = 5;
+x++;
+console.log(x);
+
+let y = 5;
+y++;
+console.log(y);
+
+let r = 5;
+r++;
+console.log(r);
+
+let f = 5;
+f++;
+console.log(f);
+
+let a = 5;
+a++;
+console.log(a);
+
+
+// #5
+let s = 5;
+s--;
+console.log(s);
+
+let d = 5;
+d--;
+console.log(d);
+
+let q= 5;
+q--;
+console.log(q);
+
+let w = 5;
+w--;
+console.log(w);
+
+let e = 5;
+e--;
+console.log(e);
+
+
+// #6
+let l = 5;
+console.log(l++);
+
+let j = 10;
+console.log(b++);
+
+let h= 0;
+console.log(h++);
+
+let m = -3;
+console.log(m++);
+
+let n = 99;
+console.log(n++);
+
+
+// #7
+let i = 5;
+console.log(++i);
+
+let o = 10;
+console.log(++o);
+
+let u= 0;
+console.log(++u);
+
+let k = -3;
+console.log(++k);
+
+let v = 99;
+console.log(++v);
+
