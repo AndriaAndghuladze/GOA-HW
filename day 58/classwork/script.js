6bae2fd2ab43d5a4e0600b113a518f1620292047
AndriaAndghuladze <andriaand999@gmail.com>
2025-04-27 18:47:10 +0400

    commit

---
+++ day 58/classwork/script.js
@@ -0,0 +1,18 @@
+// #1
+let balance = 1000000;
+let withdraw = 99999
+
+x = balance -= withdraw;
+console.log(x)
+
+
+// #2
+let Country = 'Georgia';
+let capital = 'Tblilisi';
+
+console.log(`Country  ${Country}, Capital: ${capital}`)
+
+
+// #3
+let name = prompt('enter your name:');
+console.log(`Welcome ${name}`);
---
+++ day 58/homework/script.js
@@ -0,0 +1,53 @@
+// #1
+let n1 = prompt('Enter number 1');
+let n2 = prompt('Enter number 2');
+let n3 = prompt('Enter number 3');
+let n4 = prompt('Enter number 4');
+let n5 = prompt('Enter number 5');
+
+n1 = Number(n1);
+n2 = Number(n2);
+n3 = Number(n3);
+n4 = Number(n4);
+n5 = Number(n5);
+
+console.log(n1 > n2);
+console.log(n1 < n2);
+console.log(n1 >= n3);
+console.log(n2 <= n4);
+console.log(n4 == n5);
+console.log(n3 != n5);
+console.log(n1 === n2);
+console.log(n2 !== n3);
+
+
+// #2
+console.log('hello word')
+console.log("Hello Word")
+
+let world = 'Word'
+console.log(`Hello ${world}`)
+
+
+// #3
+let balance = 1000000;
+let withdraw = 99999
+
+console.log(balance -= withdraw);
+
+
+// #4
+let Country = 'Georgia';
+let capital = 'Tblilisi';
+
+console.log(`Country  ${Country}, Capital: ${capital}`)
+
+
+// #5
+let name = prompt('enter your name:');
+console.log(`Welcome ${name}`);
+
+
+// #6
+console.log("\"Hello Word!\"")
+
