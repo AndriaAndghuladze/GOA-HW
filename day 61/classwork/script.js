59d87a5a8227eb62b5b95dfab046c939a17d6d74
AndriaAndghuladze <andriaand999@gmail.com>
2025-05-10 22:14:40 +0400

    commit

---
+++ day 61/classwork/script.js
@@ -0,0 +1,20 @@
+// #1
+for (let i = 2; i <= 50; i += 2) {
+  console.log(i);
+}
+
+
+// #2
+for (let i = 0; i <= 200; i += 2){
+    console.log(i);
+}
+
+for (let i = 0; i <= 200; i += 3){
+    console.log(i);
+}
+
+
+const i = 'GOA IS THE BEST';
+for (let i = 0; i <= 3; i++) {
+    console.log('GOA IS THE BEST');
+}
