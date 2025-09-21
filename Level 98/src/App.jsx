d2c2e8f93619ed3d3c1b4b71cad34ca0c0250e3a
AndriaAndghuladze <andriaand999@gmail.com>
2025-09-21 22:13:16 +0400

    commit

---
+++ Level 98/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <h1>🚗 მანქანების საიტი</h1>
+      <ul>
+        <li>BMW - სპორტული მანქანები</li>
+        <li>Mercedes - კომფორტი და სიჩქარე</li>
+        <li>Tesla - ელექტრო მანქანები</li>
+        <li>Audi - პრემიუმ ხარისხი</li>
+      </ul>
+    </>
+  )
+}
+
+export default App;
+
+
+
