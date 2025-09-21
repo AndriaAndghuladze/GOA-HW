9c6be7283d01a63be37e9b23a558c8bdfd1bf20c
AndriaAndghuladze <andriaand999@gmail.com>
2025-09-21 12:27:02 +0400

    commit

---
+++ Level 97/homework/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Lomi</h1>
+    </div>
+  )
+}
+
+export default App
+
+// react  არი js-ის ბიბლიოთეკა გამოიყენება ვებ აპლიკაციების ასაწყობად
