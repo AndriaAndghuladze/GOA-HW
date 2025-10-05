d622289c6bae50af8b1737be46aa6df580ec6bd5
AndriaAndghuladze <andriaand999@gmail.com>
2025-10-05 15:10:18 +0400

    commit

---
+++ Level 102/classwork/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import img from './spider.png';
+
+function App() {
+  return (
+    <>
+      <h1>Spider-Man</h1>
+
+      <img src={img} alt="Spider-Man" width="250" />
+
+      <p>
+        Spider-Man is a superhero in American comic books published by Marvel Comics.
+        Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in
+        the anthology comic book Amazing 
+        Fantasy #15 (August 1962) in the Silver Age of Comic Books.
+        Considered one of the most popular and commercially successful superheroes,
+        he has been featured in comic books, television shows, films, video games, novels, and plays.
+      </p>
+    </>
+  );
+}
+
+export default App;
