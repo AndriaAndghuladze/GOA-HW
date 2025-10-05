d91292ba9519816a72861b0e3d534de117585c12
AndriaAndghuladze <andriaand999@gmail.com>
2025-10-05 13:27:26 +0400

    commit

--- Level 101/homework/src/App.jsx
+++ Level 101/homework/src/App.jsx
@@ -1,60 +1,33 @@
+import mt from "./assets/photo.jpg";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="weather-app">
-      <header className="header">
-        <div className="logo"> Weather Now</div>
-        <button className="units-btn">Units ▾</button>
-      </header>
-
-      <h1 className="title">How’s the sky looking today?</h1>
-
-      <div className="search-bar">
-        <input type="text" placeholder="Search for a place..." />
-        <button>Search</button>
+    <div className="container">
+      <div className="left">
+        <img src={mt} alt="Perfume bottle" />
       </div>
 
-      <div className="main-info">
-        <div className="current-weather">
-          <div className="location">Berlin, Germany</div>
-          <div className="date">Tuesday, Aug 5, 2025</div>
-          <div className="temp"> 20°</div>
+      <div className="right">
+        <p className="Perfume">Perfume</p>
 
-          <div className="details">
-            <div><span>Feels Like</span><p>18°</p></div>
-            <div><span>Humidity</span><p>46%</p></div>
-            <div><span>Wind</span><p>14 km/h</p></div>
-            <div><span>Precipitation</span><p>0 mm</p></div>
-          </div>
+        <div className="text">
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by <br />
+            Olivier Polge, Perfumer-Creator <br />
+            for the House of CHANEL.
+          </p>
         </div>
 
-        <div className="hourly">
-          <h3>Hourly forecast</h3>
-          <ul>
-            <li><span>3 PM</span><span> 20°</span></li>
-            <li><span>4 PM</span><span> 20°</span></li>
-            <li><span>5 PM</span><span> 20°</span></li>
-            <li><span>6 PM</span><span> 19°</span></li>
-            <li><span>7 PM</span><span> 18°</span></li>
-            <li><span>8 PM</span><span> 18°</span></li>
-            <li><span>9 PM</span><span> 17°</span></li>
-            <li><span>10 PM</span><span> 17°</span></li>
-          </ul>
+        <div className="price">
+          <h1 className="h1">$149.99</h1>
+          <p className="p">$169.99</p>
         </div>
-      </div>
 
-      <div className="daily">
-        <h3>Daily forecast</h3>
-        <div className="days">
-          <div><span>Tue</span><p> 20° | 14°</p></div>
-          <div><span>Wed</span><p> 21° | 15°</p></div>
-          <div><span>Thu</span><p> 24° | 14°</p></div>
-          <div><span>Fri</span><p> 25° | 13°</p></div>
-          <div><span>Sat</span><p> 21° | 15°</p></div>
-          <div><span>Sun</span><p> 25° | 16°</p></div>
-          <div><span>Mon</span><p> 24° | 15°</p></div>
-        </div>
+        <button>
+           Add to Cart
+        </button>
       </div>
     </div>
   );
