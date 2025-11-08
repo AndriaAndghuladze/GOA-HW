d5c5b65ca05c91f0355f64888d754dfad5c8314c
AndriaAndghuladze <andriaand999@gmail.com>
2025-11-08 21:16:31 +0400

    commit

---
+++ Level 110/classwork/src/App.jsx
@@ -0,0 +1,41 @@
+const age = 14
+const name = "Andria"
+const items = "banana"
+const bool = true
+const level = 7
+
+export default function App() {
+  return(
+    <div>
+      <p className="one">1</p>
+      <p className="two">2</p>
+      <p className="three">3</p>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <p class="one">1</p>
+      <p class="two">2</p>
+      <p class="three">3</p>
+
+      <p>{2+3}</p>
+      <p>{2+5}</p>
+      <p>{2+88}</p>
+      <p>{2+299}</p>
+      <p>{2+299}</p>
+
+      <p>{age}</p>
+      <p>{name}</p>
+      <p>{items}</p>
+      <p>{bool}</p>
+      <p>{level}</p>
+    
+    </div>
+
+    
+    
+  )
+}
---
+++ Level 110/homework/src/App.jsx
@@ -0,0 +1,62 @@
+const age = 14
+const name = "Andria"
+const items = "banana"
+const bool = true
+const level = 7
+const age1 = 14
+const name1 = "Andria"
+const items1 = "banana"
+const bool1 = true
+const level1 = 7
+
+export default function App() {
+  return(
+    <div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+
+      <p>{2+3}</p>
+      <p>{2+5}</p>
+      <p>{2+88}</p>
+      <p>{2+299}</p>
+      <p>{2+299}</p>
+      <p>{2+3}</p>
+      <p>{2+5}</p>
+      <p>{2+88}</p>
+      <p>{2+299}</p>
+      <p>{2+299}</p>
+
+      <p>{age}</p>
+      <p>{name}</p>
+      <p>{items}</p>
+      <p>{bool}</p>
+      <p>{level}</p>
+      <p>{age1}</p>
+      <p>{name1}</p>
+      <p>{items1}</p>
+      <p>{bool1}</p>
+      <p>{level1}</p>
+
+      {/* class გამოიყენება HTML and JS, className JSX */}
+      {/* Self-closing tags მარტო ერთი თგია როგორიცაა <br /> */}
+      {/* Curly braces გამოიყენება მათემატიკური ოპერაციების შესასრულებლად */}
+      
+
+      
+    
+    </div>
+
+    
+    
+  )
+}
